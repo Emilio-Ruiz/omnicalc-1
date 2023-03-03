3ad1bb6e1c24fce976dd9fb740f8e058d68338d3
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-03 17:26:56 +0000

    commitnew

---
+++ app/controllers/square_controller.rb
@@ -0,0 +1,9 @@
+class SquareController < ApplicationController
+  def new 
+    
+  
+    render({ :template => "square/new.html.erb"})
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 
+  get("/square/new", {:controller => "square" , :action => "new"})
+
 end
