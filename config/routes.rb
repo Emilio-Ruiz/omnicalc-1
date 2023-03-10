82f681bac4407758217b3dddd53cdeedd4f648ea
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-10 20:02:16 +0000

    recent git

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get("/square_root/new", {:controller => "application", :action =>"new"})
   get("/", {:controller => "application", :action => "new"})
   get("payment/new", {:controller => "payment", :action => "new"})
-  get("random/new", {:controller => "random", :action => "new"})
-  get("square_root/results", {:controller => "application", :action => "calculate_square_root"})
-  get("payment/results", {:controller => "application", :action => "calculate_results"})
+  get("/random/new", {:controller => "random", :action => "new"})
+  get("/square_root/results", {:controller => "application", :action => "calculate_square_root"})
+  get("/payment/results", {:controller => "application", :action => "calculate_results"})
   get("/random/results", {:controller => "application", :action => "calculate_random"})
   
   
